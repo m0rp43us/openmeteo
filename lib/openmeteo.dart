7730b01b4c500309a077221012423c6e61b66248
morpheus <morpheus@localhost.localdomain>
2023-04-14 09:53:17 +0000

    putting the basic template

---
+++ lib/openmeteo.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library openmeteo;
+
+export 'src/openmeteo_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
